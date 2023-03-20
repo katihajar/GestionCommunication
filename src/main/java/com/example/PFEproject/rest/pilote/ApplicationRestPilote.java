1a17a82fb4708a0b882db39ac5ed718de09f40bc
Katihajar <hajar.kati@edu.uca.ma>
2023-03-20 15:54:38 +0000

    commit classes

---
+++ src/main/java/com/example/PFEproject/rest/pilote/ApplicationRestPilote.java
@@ -0,0 +1,26 @@
+package com.example.PFEproject.rest.pilote;
+
+import com.example.PFEproject.bean.Application;
+import com.example.PFEproject.service.ApplicationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/pilote/application")
+public class ApplicationRestPilote {
+    @Autowired
+    ApplicationService applicationService;
+    @GetMapping("/AllApp")
+    public ResponseEntity<List<Application>> findAll() {
+        return ResponseEntity.ok().body(applicationService.findAll());
+    }
+}
---
+++ src/main/java/com/example/PFEproject/rest/pilote/ProcessusMetierRestPilote.java
@@ -0,0 +1,26 @@
+package com.example.PFEproject.rest.pilote;
+
+import com.example.PFEproject.bean.ProcessusMetier;
+import com.example.PFEproject.service.ProcessusMetierService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/pilote/processusmetier")
+public class ProcessusMetierRestPilote {
+    @Autowired
+    ProcessusMetierService processusMetierService;
+    @GetMapping("/findAll")
+    public ResponseEntity<List<ProcessusMetier>> findAll() {
+        return ResponseEntity.ok().body(processusMetierService.findAll());
+    }
+}
